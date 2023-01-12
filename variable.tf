7218565822cf6c762c09c740d9e3ae9a78d9ed19
subbarao-j <113970158+subbarao-j@users.noreply.github.com>
2023-01-12 12:18:46 +0530

    add tf files

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "subnet_id_1" {
+    default = "subnet-0e5b32372ba427dc7"
+}
+
+variable "subnet_id_2" {
+    default = "subnet-025ee98c2ea5fc306"
+}
